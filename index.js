cc3ce9767dd7450abdd1650cb797934d1e09ac83
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2016-06-28 12:35:10 +0300

    add ripplr code without styles, just adding a class

---
+++ index.js
@@ -0,0 +1,39 @@
+exports.Ripplr = function() {
+  this.selector = '.btn';
+  attachClickHandlers();
+
+  function attachClickHandlers() {
+    var matches = document.querySelectorAll(this.selector);
+    for (var i=0; i < matches.length; i++) {
+      var el = matches[i];
+      el.addEventListener('click', function(e) {
+        handleClick(e, el);
+      });
+    }
+  }
+
+  function handleClick(e, el) {
+    var offset = getOffset(el),
+        div = document.createElement('div'),
+        xPos = e.pageX - offset.left,
+        yPos = e.pageY - offset.top;
+
+    div.classList.add('ripplr');
+    div.style.height = el.height;
+    div.style.width = el.height;
+    div.style.top = yPos - (parseInt(el.offsetHeight)/2);
+    div.style.left = xPos - (parseInt(el.offsetWidth)/2);
+    el.appendChild(div);
+
+    setTimeout(function() {
+      div.parentNode.removeChild(div);
+    }, 1800);
+  }
+
+  function getOffset(el) {
+    var rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+}
